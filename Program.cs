a2360ce567b401f485d241f10729e7316664ec2c
Amna Shahid <amnashahid_sh@hotmail.com>
2022-11-16 18:09:08 +0500

    check pipeline 2

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
 {
     builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
 }));
+
 var app = builder.Build();
 
 app.UseCors("corsapp");
